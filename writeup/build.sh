a2511b902d1010559a4c19d28149ce27a3e8fed2
thing 1 <thing1@seacrossedlovers.xyz>
2024-12-03 09:12:05 +0000

    used the fmt command to make it pretty

--- writeup/build.sh
+++ writeup/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 pdflatex writeup.tex
-echo "made init pdf"
 bibtex writeup.aux
-echo "created bib"
 pdflatex writeup.tex
-echo "made penutimate pdf"
 pdflatex writeup.tex
-echo "made final pdf"
